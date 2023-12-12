548ec8dac67face337198dfee03c2b96d5462cea
Sufyan <ahmed.sufyan1310@gmail.com>
2023-12-12 03:52:25 +0000

    Added code

--- Hello.java
+++ Hello.java
@@ -1,6 +1,9 @@
 public class Hello {
 	public static void main (String[] args) {
 		System.out.println("Hello World");
+		int a = 10, b = 20;
+		int sum = a+b;
+		System.out.println("The sum of a and b is "+sum);
 	}
 }
 
